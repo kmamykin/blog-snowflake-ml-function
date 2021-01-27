5bb5689d84dc869156ff65baa379cfebe68621bd
Kliment Mamykin <kmamyk@gmail.com>
2021-01-26 19:20:59 -0500

    Building a runnable docker image

--- snowflake-ml-stack/inference_service/app.py
+++ snowflake-ml-stack/inference_service/app.py
@@ -1,4 +1,8 @@
 import json
+from pathlib import Path
+import en_core_web_md
+
+nlp = en_core_web_md.load()
 
 
 def lambda_handler(event, context):
@@ -7,27 +11,31 @@ def lambda_handler(event, context):
     Parameters
     ----------
     event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-develop-integrations-lambda.html
 
     context: object, required
-        Lambda Context runtime methods and attributes
-
         Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
 
     Returns
     ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-develop-integrations-lambda.html
     """
 
+    def doc_to_ner(doc):
+        result = {
+            'entities': [{'text': ent.text, 'start_char': ent.start_char, 'end_char': ent.end_char, 'label': ent.label_}
+                         for ent in doc.ents]
+        }
+        return result
+
+    # Need to handle isBase64Encoded ?
+    body = json.loads(event['body'])
+    # Assume the body structure as an array of strings to run NER on
+    texts = body
+    docs = [doc for doc in nlp.pipe(texts, disable=["tagger", "parser"])]
     return {
         "statusCode": 200,
         "body": json.dumps(
-            {
-                "message": "hello world",
-            }
+            [doc_to_ner(doc) for doc in docs]
         ),
     }
